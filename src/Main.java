de0ece22c81e694c0145faabde07f23afe0d6765
IlieManoliUni <ili.manoli@students.uniroma2.eu>
2022-11-13 11:05:17 +0100

    Salva

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+       System.out.println("Hello");
+
+    }
+}
